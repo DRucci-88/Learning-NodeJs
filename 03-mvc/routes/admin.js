3b7214153f92853fa6525fdb4bccf3af9f033990
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-04-29 01:08:48 +0700

    MVC Complete with Enchancing the app

--- 03-mvc/routes/admin.js
+++ 03-mvc/routes/admin.js
@@ -6,13 +6,15 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const productController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 // /admin/add-product => GET
-router.get('/add-product', productController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
 // exports.products = products;
